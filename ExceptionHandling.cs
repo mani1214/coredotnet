8f6b1d2e5617d65f719527fba7ca1782b5d94693
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-15 11:20:39 +0530

    Add files via upload

---
+++ ExceptionHandling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCoreJava
+{
+    class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the number1:");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the number2:");
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(num1/num2);
+                    Console.WriteLine(num1+num2);
+                }
+                catch(FormatException fx)
+                {
+                    Console.WriteLine("please enter only number");
+                }
+               catch (DivideByZeroException fx)
+                {
+                    Console.WriteLine("num2 can't be zero");
+                }
+            }
+        }
+    }
+}
